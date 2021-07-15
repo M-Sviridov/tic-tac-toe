c4b62a4e6fa83a6609c81482df7c8e8ef57a34ee
Mathieu Sviridov <msviridov@pm.me>
2021-07-16 07:47:01 +1000

    Basic methods for updating the board, creating players, checking if there is a winner done, need to work on class arrangement

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -25,7 +25,37 @@ class GameBoard
     puts '-----------'
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+  def board_update
+    print 'Player 1, make your move: '
+    player1_selection = gets.chomp
+    @board[player1_selection] = player1.marker
+    # check if winner
+    print 'Player 2, make your move: '
+    player2_selection = gets.chomp
+    @board[player2_selection] = player2.marker
+  end
+
+  def check_if_winner
+    @board.combination(3).each do |combination|
+      winner = combination[0] if combination.all? { |x| x == combination[0] }
+      print winner
+    end
+  end
 end
 
 game_board = GameBoard.new
 game_board.print_board
+
+def create_players
+  print 'Enter name of player 1: '
+  name = gets.chomp
+  print 'Enter maker of player 1: '
+  marker = gets.chomp
+  player1 = Players.new(name, marker)
+  print 'Enter name of player 2: '
+  name = gets.chomp
+  print 'Enter maker of player 2: '
+  marker = gets.chomp
+  player2 = Players.new(name, marker)
+end
