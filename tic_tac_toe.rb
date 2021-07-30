c1a457a74d5ad504bada20c2ee7f32ff5b4eb4cc
Mathieu Sviridov <msviridov@pm.me>
2021-07-30 16:32:12 +1000

    Module for the game rules implemented, made the class "Game" responsible for the action of playing the tic tac toe, refactoring left? (forgot to commit earlier steps)

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,21 +1,60 @@
 # frozen_string_literal: true
 
-# Class representing the players of the game
-class Players
-  attr_reader :name, :marker
+# Module represeting the rules of the game (valid moves, who is winner...)
+module GameRules
+  def check_player1_move(player1_selection)
+    valid = true
+    if @board[player1_selection - 1] == @player1.marker ||
+       @board[player1_selection - 1] == @player2.marker
+      valid = false
+    elsif player1_selection < 1 || player1_selection > 9
+      valid = false
+    end
+    valid
+  end
 
-  def initialize(name, marker)
-    @name = name
-    @marker = marker
+  def check_player2_move(player2_selection)
+    valid = true
+    if @board[player2_selection - 1] == @player1.marker ||
+       @board[player2_selection - 1] == @player2.marker
+      valid = false
+    elsif player2_selection < 1 || player2_selection > 9
+      valid = false
+    end
+    valid
+  end
+
+  def check_if_winner
+    winner = false
+    winning_combinations = [
+      [board[0], board[1], board[2]], [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]], [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]], [board[2], board[5], board[8]],
+      [board[0], board[4], board[8]], [board[6], board[4], board[2]]
+    ]
+    winning_combinations.each do |combination|
+      winner = true if combination.all? { |x| x == combination[0] }
+    end
+    winner
   end
 end
 
-# Class representing the tic tac toe game board
-class GameBoard
-  attr_accessor :board
+# Class representing a player of the game
+class Players
+  attr_accessor :name, :marker
+end
 
-  def initialize
+# Class representing the action of playing the game
+class Game
+  include GameRules
+
+  attr_accessor :board
+  attr_reader :winning_combinations
+
+  def initialize(player1, player2)
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @player1 = player1
+    @player2 = player2
   end
 
   def print_board
@@ -23,39 +62,63 @@ class GameBoard
     puts '-----------'
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts '-----------'
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]}\n "
   end
 
-  def board_update
-    print 'Player 1, make your move: '
-    player1_selection = gets.chomp
-    @board[player1_selection] = player1.marker
-    # check if winner
-    print 'Player 2, make your move: '
-    player2_selection = gets.chomp
-    @board[player2_selection] = player2.marker
-  end
+  def play_game
+    while check_if_winner == false
+      player1_play
+      print_board
+      return print "#{@player1.name} is the winner!\n" if check_if_winner
 
-  def check_if_winner
-    @board.combination(3).each do |combination|
-      winner = combination[0] if combination.all? { |x| x == combination[0] }
-      print winner
+      player2_play
+      print_board
+      return print "#{@player2.name} is the winner!\n" if check_if_winner
     end
   end
+
+  def player1_play
+    print "#{@player1.name}, make your move: "
+    player1_selection = gets.chomp.to_i
+    while check_player1_move(player1_selection) == false
+      print "#{@player1.name}, make your move: "
+      player1_selection = gets.chomp.to_i
+    end
+    @board[player1_selection - 1] = @player1.marker
+    print "\n"
+  end
+
+  def player2_play
+    print "#{@player2.name}, make your move: "
+    player2_selection = gets.chomp.to_i
+    while check_player2_move(player2_selection) == false
+      print "#{@player2.name}, make your move: "
+      player2_selection = gets.chomp.to_i
+    end
+    @board[player2_selection - 1] = @player2.marker
+    print "\n"
+  end
 end
 
-game_board = GameBoard.new
-game_board.print_board
+player1 = Players.new
+print 'Player 1, enter your name: '
+name = gets.chomp
+player1.name = name
 
-def create_players
-  print 'Enter name of player 1: '
-  name = gets.chomp
-  print 'Enter maker of player 1: '
-  marker = gets.chomp
-  player1 = Players.new(name, marker)
-  print 'Enter name of player 2: '
-  name = gets.chomp
-  print 'Enter maker of player 2: '
-  marker = gets.chomp
-  player2 = Players.new(name, marker)
-end
+print "#{player1.name}, enter your marker: "
+marker = gets.chomp
+player1.marker = marker
+
+player2 = Players.new
+print 'Player 2, enter your name: '
+name = gets.chomp
+player2.name = name
+
+print "#{player2.name}, enter your marker: "
+marker = gets.chomp
+player2.marker = marker
+print "\n"
+
+game = Game.new(player1, player2)
+game.print_board
+game.play_game
